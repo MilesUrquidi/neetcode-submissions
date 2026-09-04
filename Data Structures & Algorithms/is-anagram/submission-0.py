2acabc29471e765b13a1a656e7dae6ad85447c48
Miles Urquidi <59610469+MilesUrquidi@users.noreply.github.com>
2026-09-03 22:54:24 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map = {}
+        t_map= {}
+
+        if len(s)!= len(t):
+            return False
+        
+        for char in s:
+            s_map[char] = s_map.get(char,0) + 1
+        
+        for char in t:
+            t_map[char] = t_map.get(char,0) + 1
+
+        return s_map == t_map
+
